15751a3ed7cb05bed596e98847de4212f4c83efc
hideru <jota.16.stigma.world@gmail.com>
2021-11-07 18:45:09 +0900

    wrote codes

--- WindowsFormsAppEasyCs84C/Form1.cs
+++ WindowsFormsAppEasyCs84C/Form1.cs
@@ -7,14 +7,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Schema;
 
 namespace WindowsFormsAppEasyCs84C
 {
     public partial class Form1 : Form
     {
+        private Label lb;
+        
         public Form1()
         {
             InitializeComponent();
+            this.Text = "Clock";
+            this.Width = 250;
+            this.Height = 100;
+
+            Timer tm = new Timer();
+            tm.Interval = 1000;
+            tm.Start();
+
+            lb = new Label();
+            lb.Font = new Font("Courier", 20, FontStyle.Bold);
+            lb.Dock = DockStyle.Fill;
+
+            lb.Parent = this;
+
+            tm.Tick += new EventHandler(TmTick);
+        }
+
+        public void TmTick(Object sender, EventArgs e)
+        {
+            DateTime dt = DateTime.Now;
+            lb.Text = dt.ToLongTimeString();
         }
     }
 }
